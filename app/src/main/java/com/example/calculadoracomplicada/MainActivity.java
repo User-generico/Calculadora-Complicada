0d54582494c748704f35a8c426ab1dce1c4b989c
User-generico <emailgenerico742@gmail.com>
2021-09-14 21:21:10 -0300

    final - 14-9

--- app/src/main/java/com/example/calculadoracomplicada/MainActivity.java
+++ app/src/main/java/com/example/calculadoracomplicada/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,38 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Button botao_mult;
     TextView resultado;
     EditText inserir;
-    int [] pilha1 = new int [30];
-    int [] pilha2 = new int [30];
-    //int [] soma = new int[30];
-    //int [] multiplicacao = new int[30];
-    int [] meio = new int [30];
-    int [] parcial = new int [30];
-    String convercao, soma, multiplicacao;
-    int i, j, k, l;
-    int y, f;
-    int qualmaior, vaiquanto, segura;
-    Boolean primeiro = true, vaium = false;
-
-    /*
-    * pilha1 = guarda o primeiro número digitado
-    * pilha2 = guarda o segundo numero digitado
-    * soma = gauda o resultado final do botao de soma
-    * multiplicacao = guarda o resultado final da multiplicacao
-    * meio = guarda o resultado tanto de soma quanto de multiplicacao, mas com os algarismos ao contrario
-    * parcial = guarda as somas graudias da multiplicacoes
-    * convercao = converte o edit text para os vetores pilha1 e pilha2 , ao contrário
-    * i = geralmente usado mara manter o meio
-    * j = geralmente usado para regular o fim de números nas piilhas 1 e 2
-    * k = geralmente usado para manejar pilha 1 e 2
-    * l = guarda o tamanho de pilha2
-    * y = para contar as casas que pulam na multiplicação
-    * f = Para passar os algarismos na multiplicação
-    * qualmaior = guarda o tamanho da pilha1
-    * vaiquanto = guarda a soma de vai um (ex: 3*7 = 21 } vaiquanto = 2)
-    * segura = serve como auxiliar em uma mudança no vetor (geralmente no meio)
-    * primeiro = marca se o número digitado pelko usuário é o primeiro ou o segundo
-    * vai um = confere a necessidade de um "vaium" (ex: 3*2 = 6 } vaium = false / 5*9 = 45 } vaium = true
-    */
+    Boolean primeiro = true;
+    private Stack<String> stack1 = new Stack<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,361 +30,93 @@ public class MainActivity extends AppCompatActivity {
         resultado = findViewById(R.id.resultado);
         inserir = findViewById(R.id.inserir);
 
-        for (i = 0; i < 30; i++) {
-            pilha1[i] = -1;
-            pilha2[i] = -1;
-            parcial[i] = 0;
-        }
+
     }
 
 
     public void irsoma(View f) {
-
-        convercao = String.valueOf(inserir.getText());
-        k = 29;
-        l = convercao.length();
         if (primeiro) {
-            for (j = 0; j < l; j++) {
-                pilha1[k] = convercao.charAt(j);
-                k--;
-            }
-            qualmaior = l;
+            stack1.add(String.valueOf(inserir.getText()));
             primeiro = false;
         } else {
-            for (j = 29; j < l; j++) {
-                pilha2[j] = convercao.charAt(j);
-                k--;
-            }
-            mostrars();
-            primeiro = true;
+            stack1.add(String.valueOf(inserir.getText()));
         }
-    }
-
-    public String somar(){
-        //soma//////////////////////////////////////////////////////////////////////
-        k = 29;
-        i = 0;
-        if (qualmaior >= l) {
-            j = pilha1[k];
+        char[] a, b;
+        int[] c = new int[30];
+        String passando = "";
+        String temp2 = "", temp1 = "";
+        String pop2;
+        pop2 = stack1.pop();
+        String pop1 = stack1.pop();
+        a = pop1.toCharArray();
+        b = pop2.toCharArray();
+        for (int i = pop1.length() - 1; i >= 0; i--) {
+            temp1 = temp1 + a[i];
+        }
+        for (int i = pop2.length() - 1; i >= 0; i--) {
+            temp2 = temp2 + b[i];
+        }
+        a = temp1.toCharArray();
+        b = temp2.toCharArray();
+        for (int i = 0; i < pop1.length(); i++) {
+            c[i] = c[i] + Character.getNumericValue(a[i]);
+        }
+        for (int i = 0; i < pop2.length(); i++) {
+            c[i] = c[i] + Character.getNumericValue(b[i]);
+            c[i + 1] = c[i + 1] + (c[i] / 10);
+            c[i] = c[i] - (10 * (c[i] / 10));
+        }
+        if (pop1.length() > pop2.length()) {
+            if (c[pop1.length()] != 0) {
+                passando += Integer.toString(c[pop1.length()]);
+            }
+            for (int i = pop1.length() - 1; i >= 0; i--) {
+                passando = passando + Integer.toString(c[i]);
+            }
         } else {
-            j = pilha2[k];
-        }
-        while (j != -1) {
-            if (pilha1[k] != -1 && pilha2[k] != -1) {
-                if (pilha1[k] + pilha2[k] < 10) {
-                    meio[i] = pilha1[k] + pilha2[k];
-                    if (vaium) {
-                        meio[i] = meio[i] + 1;
-                        if (meio[i] == 10) {
-                            meio[i] = 0;
-                            vaium = true;
-                        } else {
-                            vaium = false;
-                        }
-                    }
-                } else {
-                    meio[i] = ((pilha1[k] + pilha2[k]) % 10);
-                    if (vaium) {
-                        meio[i] = meio[i] + 1;
-                    }
-                    vaium = true;
-                }
-            } else {
-                if (qualmaior >= l) {
-                    meio[i] = pilha1[k];
-                } else {
-                    meio[i] = pilha2[k];
-                }
-                if (vaium) {
-                    meio[i] = meio[i] + 1;
-                    if (meio[i] == 10) {
-                        meio[i] = 0;
-                        vaium = true;
-                    } else {
-                        vaium = false;
-                    }
-                }
+            if (c[pop2.length()] != 0) {
+                passando += Integer.toString(c[pop2.length()]);
             }
-            k--;
-            i++;
-            if (qualmaior >= l) {
-                j = pilha1[k];
-            } else {
-                j = pilha2[k];
+            for (int i = pop2.length() - 1; i >= 0; i--) {
+                passando = passando + Integer.toString(c[i]);
             }
         }
+        stack1.push(passando);
+        resultado.setText(stack1.peek());
+        primeiro = true;
 
-        //inverte meio para soma (que vai ser mostrada na view)
-        k = meio.length - 1;
-        for (i = 0; i < meio.length; i++) {
-            soma = String.valueOf(meio[k]);
-            k--;
-        }
-        return soma;
     }
 
-    public void mostrars() {
-        somar();
-        resultado.setText(somar());
-    }
 
 
     public void irmult(View f) {
-
-        convercao = String.valueOf(inserir.getText());
-        k = 29;
-        l = convercao.length();
         if (primeiro) {
-            for (j = 0; j < l; j++) {
-                pilha1[k] = convercao.charAt(j);
-                k--;
-            }
-            qualmaior = l;
+            stack1.add(String.valueOf(inserir.getText()));
             primeiro = false;
         } else {
-            for (j = 29; j < l; j++) {
-                pilha2[j] = convercao.charAt(j);
-                k--;
-            }
-            mostrarm();
+            stack1.add(String.valueOf(inserir.getText()));
         }
-    }
-
-    public void mostrarm() {
-        somar();
-        resultado.setText(multiplicar());
-    }
-
-    public String multiplicar(){
-        //multiplicação///////////////////////////////////////////////////////////////////////
-        k = 29;
-        i = 0;
-        if (qualmaior >= l) {
-            j = (pilha1[k]);
-        } else {
-            j = (pilha2[k]);
-        }
-        y = 0;
-        while (y <= qualmaior) {
-            k = 29;
-            i = 0;
-            f = 29;
-            while (j != -1) {
-                if (pilha1[k] != -1 && pilha2[f] != -1) {
-                    if (pilha1[k] * pilha2[f] < 10) {
-                        meio[i] = pilha1[k] * pilha2[f];
-                    } else {
-                        meio[i] = ((pilha1[k] * pilha2[f]) % 10);
-                        vaiquanto = ((pilha1[k] * pilha2[f]) / 10);
-                    }
-                } else {
-                    if (qualmaior >= l) {
-                        meio[i] = pilha1[k];
-                    } else {
-                        meio[i] = pilha2[f];
-                    }
-                }
-                if (vaium) {
-                    switch (vaiquanto) {
-                        case 1:
-                            meio[i] = meio[i] + 1;
-                            if (meio[i] >= 10) {
-                                segura = (meio[i]);
-                                meio[i] = (meio[i] % 10);
-                                vaium = true;
-                                vaiquanto = (segura / 10);
-                            } else {
-                                vaium = false;
-                            }
-                            break;
-                        case 2:
-                            meio[i] = meio[i] + 2;
-                            if (meio[i] >= 10) {
-                                segura = meio[i];
-                                meio[i] = (meio[i] % 10);
-                                vaium = true;
-                                vaiquanto = (segura / 10);
-                            } else {
-                                vaium = false;
-                            }
-                            break;
-                        case 3:
-                            meio[i] = meio[i] + 3;
-                            if (meio[i] >= 10) {
-                                segura = meio[i];
-                                meio[i] = (meio[i] % 10);
-                                vaium = true;
-                                vaiquanto = (segura / 10);
-                            } else {
-                                vaium = false;
-                            }
-                            break;
-                        case 4:
-                            meio[i] = meio[i] + 4;
-                            if (meio[i] >= 10) {
-                                segura = meio[i];
-                                meio[i] = (meio[i] % 10);
-                                vaium = true;
-                                vaiquanto = (segura / 10);
-                            } else {
-                                vaium = false;
-                            }
-                            break;
-                        case 5:
-                            meio[i] = meio[i] + 5;
-                            if (meio[i] >= 10) {
-                                segura = meio[i];
-                                meio[i] = (meio[i] % 10);
-                                vaium = true;
-                                vaiquanto = (segura / 10);
-                            } else {
-                                vaium = false;
-                            }
-                            break;
-                        case 6:
-                            meio[i] = meio[i] + 6;
-                            if (meio[i] >= 10) {
-                                segura = meio[i];
-                                meio[i] = (meio[i] % 10);
-                                vaium = true;
-                                vaiquanto = (segura / 10);
-                            } else {
-                                vaium = false;
-                            }
-                            break;
-                        case 7:
-                            meio[i] = meio[i] + 7;
-                            if (meio[i] >= 10) {
-                                segura = meio[i];
-                                meio[i] = (meio[i] % 10);
-                                vaium = true;
-                                vaiquanto = (segura / 10);
-                            } else {
-                                vaium = false;
-                            }
-                            break;
-                        case 8:
-                            meio[i] = meio[i] + 8;
-                            if (meio[i] >= 10) {
-                                segura = meio[i];
-                                meio[i] = (meio[i] % 10);
-                                vaium = true;
-                                vaiquanto = (segura / 10);
-                            } else {
-                                vaium = false;
-                            }
-                            break;
-                    }
-                }
-                k--;
-                i++;
-                if (qualmaior >= l) {
-                    j = pilha1[k];
-                } else {
-                    j = pilha2[f];
+        char[] a, b;
+        int[] c = new int[30];
+        String passando = "";
+        String pop2 = stack1.pop();
+        String pop1 = stack1.pop();
+        b = pop2.toCharArray();
+        a = pop1.toCharArray();
+        for (int i = pop1.length() - 1; i >= 0; i--) {
+            for (int j = pop2.length() - 1; j >= 0; j--) {
+                c[j + i] = c[j + i] + Character.getNumericValue(a[i]) * Character.getNumericValue(b[j]);
+                if (j + i - 1 >= 0) {
+                    c[j + i - 1] = c[j + i - 1] + (c[j + i] / 10);
+                    c[j + i] = c[j + i] - (10 * (c[j + i] / 10));
                 }
             }
-            if ( y > 1){
-                parcial = somar2(pilha1, pilha2, parcial, y);
-            }
-            f--;
-            y++;
         }
+        for (int i = 0; i <= pop2.length() + pop1.length() - 2; i++) {
+            passando = passando + Integer.toString(c[i]);
+        }
+        stack1.push(passando);
+        resultado.setText(stack1.peek());
         primeiro = true;
-
-
-        //inverte meio para multiplicação (que vai ser mostrada na view)
-        k = meio.length - 1;
-        for (i = 0; i < meio.length; i++) {
-            multiplicacao = String.valueOf(meio[k]);
-            k--;
-        }
-        return multiplicacao;
-
     }
-
-    public int[] somar2( int []pilha1, int []pilha2, int []meio, int pular){
-        int segura;
-        y = pular;
-        k = 29;
-        i = 0;
-        l = pilha2.length;
-        int h;
-        qualmaior = pilha1.length;
-        if (qualmaior >= l) {
-            j = pilha1[k];
-        } else {
-            j = pilha2[k];
-        }
-        //vê quantas casas é preciso pular na soma das multiplicações um a um
-        while (y > 0){
-            segura = pilha2[k];
-            pilha2[k] = 0;
-            k--;
-            h = k;
-            while (segura != -1){
-                pilha2[h] = segura;
-                h--;
-                segura = pilha2[h];
-            }
-            y--;
-        }
-        k = 29;
-        //realiza a soma das pilhas
-        while (j != -1) {
-            if (pilha1[k] != -1 && pilha2[k] != -1) {
-                if (pilha1[k] + pilha2[k] < 10) {
-                    meio[i] = pilha1[k] + pilha2[k];
-                    if (vaium) {
-                        meio[i] = meio[i] + 1;
-                        if (meio[i] == 10) {
-                            meio[i] = 0;
-                            vaium = true;
-                        } else {
-                            vaium = false;
-                        }
-                    }
-                } else {
-                    meio[i] = ((pilha1[k] + pilha2[k]) % 10);
-                    if (vaium) {
-                        meio[i] = meio[i] + 1;
-                    }
-                    vaium = true;
-                }
-            } else {
-                if (qualmaior >= l) {
-                    meio[i] = pilha1[k];
-                } else {
-                    meio[i] = pilha2[k];
-                }
-                if (vaium) {
-                    meio[i] = meio[i] + 1;
-                    if (meio[i] == 10) {
-                        meio[i] = 0;
-                        vaium = true;
-                    } else {
-                        vaium = false;
-                    }
-                }
-            }
-            k--;
-            i++;
-            if (qualmaior >= l) {
-                j = pilha1[k];
-            } else {
-                j = pilha2[k];
-            }
-        }
-
-        //inverte meio para soma (que vai ser mostrada na view)
-        k = meio.length - 1;
-        for (i = 0; i < meio.length; i++) {
-            soma = String.valueOf(meio[k]);
-            k--;
-        }
-        return meio;
-    }
-
 }
-
-
